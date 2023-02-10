bb6f95f6395f08dc6dcc1c3b8c70c89deeaf15f5
Dmitry Marakov <dmarakov@devexperts.com>
2023-02-10 11:28:24 +0400

    Fixed checkstyle error

--- src/main/java/ru/job4j/search/Task.java
+++ src/main/java/ru/job4j/search/Task.java
@@ -4,7 +4,6 @@ public class Task {
     private String desc;
     private int priority;
 
-
     public Task(String desc, int priority) {
         this.desc = desc;
         this.priority = priority;
