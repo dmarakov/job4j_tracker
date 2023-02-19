6ffc6fbef78746c305a90f6315922b67bcf70fa4
Dmitry Marakov <dmarakov@devexperts.com>
2023-02-20 01:44:49 +0400

    Added elements to map

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("helloworld@gmail.com", "Ivanov Ivan Ivanovich");
+        map.put("test@gmail.com", "Petrov Petr");
+        map.put("helloworld@gmail.com", "Nikolaev Nikolay");
+        map.put("hellotest@gmail.com", "AlexandrovAlexandr");
+        map.put("test@gmail.com", "Stepanov Stepan");
         for (String name : map.keySet()) {
             System.out.println(map.get(name));
         }
