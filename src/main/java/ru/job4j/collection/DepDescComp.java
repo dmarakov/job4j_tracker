dac7bcb7cba23edf6742d717e6969d491c5bcd41
Dmitry Marakov <dmarakov@devexperts.com>
2023-03-04 23:07:02 +0400

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] o1Array = o1.split("/");
+        String[] o2Array = o2.split("/");
+        int rsl = o1Array[0].compareTo(o2Array[0]);
+        if (rsl == 0) {
+            rsl = o1.compareTo(o2);
+        } else {
+            rsl = o2.compareTo(o1);
+        }
+        return rsl;
+    }
+}
---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String dep : deps) {
+            StringBuilder start = new StringBuilder();
+            for (String str : dep.split("/")) {
+                tmp.add(start + str);
+                start.append(str).append("/");
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
+    }
+}
