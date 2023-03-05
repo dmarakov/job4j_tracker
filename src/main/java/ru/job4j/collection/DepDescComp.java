5d5cb78b154284660f962724ad32c39dcf00321f
Dmitry Marakov <dmarakov@devexperts.com>
2023-03-05 14:56:40 +0400

    Fixed DepDescComp

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,12 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] o1Array = o1.split("/");
         String[] o2Array = o2.split("/");
-        int rsl = o1Array[0].compareTo(o2Array[0]);
-        if (rsl == 0) {
-            rsl = o1.compareTo(o2);
-        } else {
-            rsl = o2.compareTo(o1);
-        }
-        return rsl;
+        int rsl = o2Array[0].compareTo(o1Array[0]);
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
