646279b9ad6ae14162629956d55b9f90b82f5b39
Dmitry Marakov <dmarakov@devexperts.com>
2023-03-05 14:53:30 +0400

    Fixed argument order

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -9,7 +9,7 @@ public class LambdaUsage {
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("eeeee", "a", "ccc", "dddd", "bb");
         Comparator<String> comparator = (left, right) -> {
-            System.out.println("Длинна сравниваемых строк: " + right.length() + " и " + left.length());
+            System.out.println("Длина сравниваемых строк: " + left.length() + " и " + right.length());
             return Integer.compare(right.length(), left.length());
         };
         strings.sort(comparator);
