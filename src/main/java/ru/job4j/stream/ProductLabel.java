e6bcf1f556fa0d21a339d202e6f8b6361c3adcb1
Dmitry Marakov <dmarakov@devexperts.com>
2023-03-11 12:57:37 +0400

    Added one more condition

--- src/main/java/ru/job4j/stream/ProductLabel.java
+++ src/main/java/ru/job4j/stream/ProductLabel.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class ProductLabel {
     public List<String> generateLabels(List<Product> products) {
         return products.stream()
-                .filter(it -> it.getStandard() - it.getActual() <= 3)
+                .filter(it -> it.getStandard() - it.getActual() <= 3 && it.getStandard() - it.getActual() >= 0)
                 .map(it -> new Label(it.getName(), (it.getPrice() * 0.5f)).toString())
                 .toList();
     }
