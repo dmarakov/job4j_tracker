a01af49f35b913ba18c6702ef1b4d052ca9bf865
Dmitry Marakov <dmarakov@devexperts.com>
2023-03-15 10:51:37 +0400

    Fixed Checkstyle error

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -28,9 +28,8 @@ public class Card {
 
     @Override
     public String toString() {
-        return "Card{" +
-                "suit=" + suit +
-                ", value=" + value +
-                '}';
+        return "Card{"
+                + "suit=" + suit
+                + ", value=" + value + '}';
     }
 }
