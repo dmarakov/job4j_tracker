ea8038191161ed835de20f78ea04da096506fd4a
Dmitry Marakov <dmarakov@devexperts.com>
2023-06-16 13:16:09 +0400

    - add generatedKeys value and logic for it

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -43,10 +43,15 @@ public class SqlTracker implements Store {
 
     @Override
     public Item add(Item item) {
-        try (PreparedStatement ps = cn.prepareStatement("INSERT INTO items(name,created) VALUES(?,?)")) {
+        try (PreparedStatement ps = cn.prepareStatement("INSERT INTO items(name,created) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, item.getName());
             ps.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
             ps.execute();
+            try ( ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()){
+                    item.setId(generatedKeys.getInt(1));
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
